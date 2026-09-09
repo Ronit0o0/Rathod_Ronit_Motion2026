f9520118e503af25a0095a8d857a6b2675d88306
Ronit0o0 <rathroni@sheridancollege.ca>
2026-09-09 08:52:41 -0400

    Started work on problem 1

---
+++ Rathod_Ronit_Motion2026/Assets/Scripts/Problem 1.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Problem1 : MonoBehaviour
+{
+    public Vector2 point1;
+    public Vector2 point2;
+    public Vector2 point3;
+    public Vector2 point4;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point1plus2 = point1 + point2;
+
+        
+    }
+}
