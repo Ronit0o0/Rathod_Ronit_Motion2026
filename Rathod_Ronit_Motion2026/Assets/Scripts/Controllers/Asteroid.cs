38f77d8617ef6435dc007f9da6c2cc99613b362d
Ronit0o0 <ronit.rathod@torontofilmschool.ca>
2026-09-25 18:36:23 -0400

    Completed Task2 went over the time limit but managed to figure out the issue

--- Rathod_Ronit_Motion2026/Assets/Scripts/Controllers/Asteroid.cs
+++ Rathod_Ronit_Motion2026/Assets/Scripts/Controllers/Asteroid.cs
@@ -7,14 +7,43 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    Vector3 randomPoint;
 
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+
+        Vector3 randomDirectionNorm = randomDirection.normalized * maxFloatDistance;
+
+        randomPoint = transform.position + randomDirectionNorm;
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    void AsteroidMovement()
+    {
+       
+
+        Vector3 directonToTarget = randomPoint - transform.position;
+
+        float directionToTargetMag = directonToTarget.magnitude;
+
+        if (directionToTargetMag > arrivalDistance)
+        {
+            transform.position += directonToTarget.normalized * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+
+            Vector3 randomDirectionNorm = randomDirection.normalized * maxFloatDistance;
+
+            randomPoint = transform.position + randomDirectionNorm;
+        }
     }
 }
